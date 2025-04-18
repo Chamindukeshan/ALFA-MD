60733ece63cf903e122e62a40f87c9d34dc6c9f7
Chamindukeshan <Chamindukeshankeshan233@gmail.com>
2025-04-18 10:46:35 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await ALFAPairweb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/Dark-IMG-20250418-WA0003.7z",
               },
               caption: sid,
             });
